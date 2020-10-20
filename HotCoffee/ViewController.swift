e1ac469aa9e3e86e116c6c7cf0b3f83486b6af39
Riad Mohamed <riaduk1@gmail.com>
2020-10-20 18:10:53 +0200

    Initial Commit

---
+++ HotCoffee/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HotCoffee
+//
+//  Created by Riad Mohamed on 10/20/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
