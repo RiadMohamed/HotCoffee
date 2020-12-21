fccacc134c5a0947d5cf32c9fb349bf676d339d1
Riad Mohamed <riaduk1@gmail.com>
2020-12-21 21:34:13 +0200

    Create the Order class/model

---
+++ HotCoffee/Models/Order.swift
@@ -0,0 +1,27 @@
+//
+//  Order.swift
+//  HotCoffee
+//
+//  Created by Riad Mohamed on 12/21/20.
+//
+
+import Foundation
+
+enum CoffeeType: String, Codable {
+    case vanilla
+    case caramel
+    case mocha
+}
+
+enum CoffeeSize: String, Codable {
+    case small
+    case medium
+    case large
+}
+
+struct Order: Codable {
+    let name: String
+    let email: String
+    let type: CoffeeType
+    let size: CoffeeSize
+}
